0de86258c2f86de9acc5b6326d8167eef04c8865
panchen <panchen@tigerbrokers.com>
2021-08-27 17:14:07 +0800

    refactor.

---
+++ library/src/main/java/me/bytebeats/views/bankcard/FormatEditText.kt
@@ -0,0 +1,33 @@
+package me.bytebeats.views.bankcard
+
+import androidx.appcompat.widget.AppCompatEditText
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+
+/**
+ * @author bytebeats || happychinapc[at]gmail[dot]com
+ * @date 2015/3/25
+ * @time 16:58
+ */
+class FormatEditText : AppCompatEditText {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    private val mTextWatcher by lazy { FormatTextWatcher(this) }
+
+    private fun init(attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.FormatEditText)
+        val splitterIdx = typedArray.getInt(R.styleable.FormatEditText_splitter, 0)
+        mTextWatcher.splitter = Splitter.values()[splitterIdx]
+        typedArray.recycle()
+        addTextChangedListener(mTextWatcher)
+    }
+
+    val splitter: Splitter
+        get() = mTextWatcher.splitter
+}
---
+++ library/src/main/java/me/bytebeats/views/bankcard/Splitter.kt
@@ -0,0 +1,14 @@
+package me.bytebeats.views.bankcard
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2021/8/27 16:31
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+enum class Splitter(val splitter: Char) {
+    WHITESPACE(' '), POUND('#'), HYPHEN('-')
+}
